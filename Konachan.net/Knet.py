f4b6c8c16f98e94ca693722910d1fe515d7df4bd
Pengyi Mu <mupengyi@foxmail.com>
2018-02-02 16:44:25 +0800

    Update Knet.py

--- Konachan.net/Knet.py
+++ Konachan.net/Knet.py
@@ -10,6 +10,20 @@ import urllib.request
 def GetPicStyleName():
     PicStyle = str(input("请输入要在K站爬取图片的类型：\n"))
     #HomeUrl = "https://konachan.com/post?tags=" + PicStyle
+    #创建文件夹，并将工作目录设置为该文件夹.
+    
+    FilePath = "D:\\konachan\\"+ PicStyle + '\\'
+    if not (os.path.exists(FilePath)):
+        os.makedirs(FilePath)
+    else:
+        Tmp = input('已存在该文件记录,是否继续？请输入[继续\\退出]:\n')
+        while Tmp != '继续' and Tmp != '退出':
+            Tmp = input("\n指令有误,请重新输入:")
+
+        if Tmp == '退出':
+            return 0
+    os.chdir(FilePath)
+    
     return PicStyle
 
 def OpenHomeUrl(Url):
@@ -38,6 +52,7 @@ def CopyPic(PicResult):
 
         #如果其不存在类似链接,迭代获取下一个
         if not link:
+            print('pass')
             continue
         link = link.group()
         Response = urllib.request.urlopen(link)
@@ -48,9 +63,9 @@ def CopyPic(PicResult):
         if os.path.exists(FileName):
             continue
         
-        with open(FileName,'wb') as File:
-            File.write(Response.read())
-            File.close()
+        with open(FileName,'wb') as F:
+            F.write(Response.read())
+            F.close()
             count = count+1
         print("第" + str(count) + "张"+"下载完成!")
         
@@ -65,12 +80,25 @@ if __name__ == '__main__':
     os.chdir(Path)
     Tags = GetPicStyleName()
     HomeUrl = "https://konachan.com/post?tags=" + Tags
-    
-    Page = 1
+    #打开当前文件夹内是否有Page.dat(用于指示爬取到某一页)
+    if((os.path.exists('Page.dat'))):
+        Tmp = open('Page.dat')
+        Page = int(Tmp.read())
+        Tmp.close()
+    else:
+        Page = 1
+        
     We = OpenHomeUrl(HomeUrl)
     while(We != 0):
         CopyPic(We)
         Page = Page+1
         HomeUrl = 'https://konachan.com/post?' + 'page=' + str(Page) + '&tags=' + Tags
+        
         We = OpenHomeUrl(HomeUrl)
-        time.sleep(5)   #防止访问过频，然后GG
+        #写入爬取K站的页面的页数
+        with open('Page.dat','wb') as T:
+            SomeThing = str(Page)
+            T.write(SomeThing)
+            T.close()
+        
+        time.sleep(1)   #防止访问过频，然后GG
